cad2671c1b65cbdd5dae3530b61a2d3f72a9f36c
Limon Roy Apu <limonroyapu.edu@gmail.com>
2026-05-19 16:10:35 +0600

    Update YouDo3.py

--- Task 1/YouDo3.py
+++ Task 1/YouDo3.py
@@ -1,7 +1,11 @@
 a = 20
 b = 7
 
-if a*b > 50:
-    print("True")
-else:
-    print("False")
+# if a*b > 50:
+#     print("True")
+# else:
+#     print("False")
+
+mul = a*b
+is_greater = mul > 50
+print("Multiplication of a and b is greater than 50: ", is_greater)
