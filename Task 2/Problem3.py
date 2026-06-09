8b3b50512c2aaecf02e57ce62b604d8ebee5034e
Limon Roy Apu <150415797+limon-l@users.noreply.github.com>
2026-06-09 16:03:33 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
+pattern = input().strip()
 
+complement = {
+    'A' : 'T',
+    'T' : 'A',
+    'C' : 'G',
+    'G' : 'C'
+}
+
+reverse_complement = ""
+
+for nucleotide in reversed(pattern):
+    reverse_complement += complement[nucleotide]
+
+print(reverse_complement)
