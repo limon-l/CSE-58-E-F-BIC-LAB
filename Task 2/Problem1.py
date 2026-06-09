4ca8033937fb830029eb34fca6b7da566e7a459b
Limon Roy Apu <150415797+limon-l@users.noreply.github.com>
2026-06-09 16:07:33 +0600

    Update Problem1.py

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -1,1 +1,10 @@
+text = input().strip()
+pattern = input().strip()
 
+count = 0
+
+for i in range(len(text) - len(pattern) + 1):
+    if text[i:i + len(pattern)] == pattern:
+        count += 1
+
+print(count)
