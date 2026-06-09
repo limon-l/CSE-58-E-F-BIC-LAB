bbf8f9e2d1dd8cf2288ab26ac7709c9787e6f2fb
Limon Roy Apu <150415797+limon-l@users.noreply.github.com>
2026-06-09 16:10:22 +0600

    Update Problem4.py

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,10 @@
+pattern = input().strip()
+genome = input().strip()
 
+result = []
+
+for i in range(len(genome) - len(pattern) + 1):
+    if genome[i:i + len(pattern)] == pattern:
+        result.append(str(i))
+
+print(" ".join(result))
