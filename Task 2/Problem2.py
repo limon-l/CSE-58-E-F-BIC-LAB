edc6a24faeff241142e47fb294fe0dc20f678ef2
Limon Roy Apu <150415797+limon-l@users.noreply.github.com>
2026-06-09 15:58:53 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,18 @@
+text = input().strip()
+k = int(input())
 
+frequency = {}
+
+for i in range(len(text) - k + 1):
+    pattern = text[i:i+k]
+    frequency[pattern] = frequency.get(pattern, 0) + 1
+
+max_count = max(frequency.values())
+
+result = []
+
+for pattern, count in frequency.items():
+        if count == max_count:
+            result.append(pattern)
+
+print(*result)
