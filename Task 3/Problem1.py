8306ff20a850c6b70fc92d8d45e100fb6c28fc25
Limon Roy Apu <150415797+limon-l@users.noreply.github.com>
2026-06-16 16:12:56 +0600

    Update Problem1.py

--- Task 3/Problem1.py
+++ Task 3/Problem1.py
@@ -1,1 +1,21 @@
+def mismatch(a, b):
+    return sum(x != y for x, y in zip(a,b))
 
+def approximate_pattern_matching(pattern, text, d):
+    k = len(pattern)
+    positions = []
+    
+    for i in range(len(text) -k +1):
+        window = text[i:i+k]
+        
+        if mismatch(pattern, window) <= d:
+            positions.append(i)
+    return positions
+
+pattern = input().strip()
+text = input().strip()
+d = int(input().strip())
+
+result = approximate_pattern_matching(pattern, text, d)
+
+print(*result)
